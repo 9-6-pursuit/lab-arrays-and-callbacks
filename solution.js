4d44c54183099b6753c38bf30d488b348e1a4e1d
antunishdPursuit <dennysantunish@pursuit.org>
2023-09-14 00:34:58 -0400

    Completed

--- solution.js
+++ solution.js
@@ -1,72 +1,98 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
+const isEveryNumGreaterThan2 = (arrNum) => {
   //
+  return arrNum.every(ele => ele >=2)
 };
 
-const isEveryWordShorterThan7 = () => {
+const isEveryWordShorterThan7 = (arrWords) => {
   //
+  return arrWords.every(ele => ele.length < 7)
+
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
+const arrayLessThan5 = (arrWords) => {
   //
+  return arrWords.filter(ele => ele < 5)
 };
 
-const arrayOddLengthWords = () => {
+const arrayOddLengthWords = (arrWords) => {
   //
+  return arrWords.filter(ele => ele.length % 2 !== 0)
+
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
+const firstValDivisibleBy4 = (arrNum) => {
   //
+  return arrNum.find(ele => ele % 4 === 0)
+
 };
 
-const firstWordLongerThan4Char = () => {
+const firstWordLongerThan4Char = (arrWords) => {
   //
+  return arrWords.find(ele => ele.length > 4)
+
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
+const firstNumIndexDivisibleBy3 = (arrNum) => {
   //
+  return arrNum.findIndex(ele => ele % 3 === 0)
+
 };
 
-const firstWordIndexLessThan2Char = () => {
+const firstWordIndexLessThan2Char = (arrWords) => {
   //
+  return arrWords.findIndex(ele => ele.length <2)
+
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
+const logValuesTimes3 = (arrNum) => {
   //
+  return arrNum.forEach(ele => console.log(ele * 3))
+
 };
 
-const logWordsWithExclamation = () => {
+const logWordsWithExclamation = (arrWords) => {
   //
+  return arrWords.forEach(ele => console.log(ele+"!"))
+
 };
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
+const arrayValuesSquaredTimesIndex = (arrNum) => {
   //
+  return arrNum.map((ele, i) => (ele * ele) * i)
+
 };
 
-const arrayWordsUpcased = () => {
+const arrayWordsUpcased = (arrWords) => {
   //
+  return arrWords.map(ele => ele.toUpperCase())
+
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
+const areSomeNumsDivisibleBy7 = (arrNum) => {
   //
+  return arrNum.some(ele => ele % 7 === 0) 
+
 };
 
-const doSomeWordsHaveAnA = () => {
+const doSomeWordsHaveAnA = (arrWords) => {
   //
+  return arrWords.some(ele => ele.includes("a"))
+
 };
 
 module.exports = {
