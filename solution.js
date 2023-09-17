9ff51b448c8be8a99aa1785baa7ccdb9c38fd2d4
Ari-So-Irie <arius1500@gmail.com>
2023-09-17 12:09:38 -0400

    lab complete

--- solution.js
+++ solution.js
@@ -1,72 +1,94 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+// Is every number is greater than or equal to 2
+const isEveryNumGreaterThan2 = (nums) => {
+ let result = nums.every(num => num >= 2)
+  return result
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+//Is every word is shorter than 7 characters
+const isEveryWordShorterThan7 = (words) => {
+  let result = words.every(word => word.length < 7)
+  return result
 };
 
 // Filter
-
-const arrayLessThan5 = () => {
-  //
+//A new array with all values less than 5 
+const arrayLessThan5 = (nums) => {
+  let result = nums.filter(num => num < 5) 
+  return result
 };
 
-const arrayOddLengthWords = () => {
-  //
+//A new array with all words with odd lengths
+const arrayOddLengthWords = (words) => {
+  let result = words.filter(word => word.length % 2 === 1)
+  return result
 };
 
 // Find
-
-const firstValDivisibleBy4 = () => {
-  //
+//Find first value that is divisible by 4
+const firstValDivisibleBy4 = (nums) => {
+  let result = nums.find(num => num % 4 === 0)
+  return result
 };
 
-const firstWordLongerThan4Char = () => {
-  //
+//Find first word longer than 4 characters
+const firstWordLongerThan4Char = (words) => {
+  let result = words.find(word => word.length > 4)
+  return result
 };
 
 // Find Index
-
-const firstNumIndexDivisibleBy3 = () => {
-  //
+//Find the index of the first number that is divisible by 3
+const firstNumIndexDivisibleBy3 = (nums) => {
+  let result = nums.findIndex(num => num % 3 === 0)
+  return result
 };
 
-const firstWordIndexLessThan2Char = () => {
-  //
+//Find the index of the first word that is less than 2 characters long
+const firstWordIndexLessThan2Char = (words) => {
+  let result = words.findIndex(word => word < 2)
+  return result
 };
 
 // For Each
-
-const logValuesTimes3 = () => {
-  //
+//Console log the values times 3
+const logValuesTimes3 = (nums) => {
+  let result = nums.forEach(num => num * 3)
+  return result
 };
 
-const logWordsWithExclamation = () => {
-  //
+//Console log each word with an exclamation point
+const logWordsWithExclamation = (words) => {
+  let result = words.forEach(word => word.includes("!"))
+  return result
 };
 
 // Map
+//A new array of values that are squared and then multiplied by their index number
+const arrayValuesSquaredTimesIndex = (nums) => {
+  const square = (number) => number**2;
+  let result = nums.map((num, index)=> (square(num)*index))
+  return result
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
 };
-
-const arrayWordsUpcased = () => {
-  //
+//A new array of words that are all uppercase
+const arrayWordsUpcased = (words) => {
+let result = words.map((word)=> word.toUpperCase())
+return result
 };
 
 // Some
-
-const areSomeNumsDivisibleBy7 = () => {
-  //
+//Are some numbers divisible  by 7?
+const areSomeNumsDivisibleBy7 = (nums) => {
+  let result = nums.some(num => num % 7 === 0)
+  return result
 };
-
+//Do some words have an a?
 const doSomeWordsHaveAnA = () => {
-  //
+  let result = words.some(word => word.includes("a"))
+  return result
 };
 
 module.exports = {
